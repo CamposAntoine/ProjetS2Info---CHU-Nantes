07d090d1168fbe75d3617c72633e98d97b134cca
Matti Comba <matti.comba@etu.minesparis.psl.eu>
2024-03-12 16:00:40 +0100

    Page login

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, session, request, redirect
+    
 app = Flask(__name__)
 
 @app.route("/")
 def get_menu():
     return render_template("index.html")
+
+database={'admin': 'admin'}
+
+@app.route("/login/")
+def login():
+    return render_template("login.html")
+
+@app.route('/form_login', methods=['POST', 'GET'])
+def form_login():
+    id= request.form['username']
+    mdp= request.form['password']
+    if id not in database:
+        return render_template("login.html", message="Votre nom d'utilisateur est invalide.")
+    else:
+        if database[id]!=mdp:
+            render_template("login.html", message="Votre mot de passe et votre nom d'utilisateur ne correspondent pas.")
+        else:
+            return redirect("/admin")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
